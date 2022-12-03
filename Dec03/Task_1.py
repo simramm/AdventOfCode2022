3ddd6660a01bdcd54a13a7d3f9057e25ab94bb8c
simramm <66185123+simramm@users.noreply.github.com>
2022-12-03 06:52:25 +0100

    Completed Dec03

--- Dec03/Task_1.py
+++ Dec03/Task_1.py
@@ -1,6 +1,7 @@
 import os
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+#
 f = open(os.path.join(__location__,"Input.txt"))
 
 # if test needed
@@ -10,13 +11,24 @@ input_list = f.read().split("\n")
 
 #input_list = f.read().split()
 #input_list = list(map(int, f.read().split('\n')))
+
 #input_list = list(map(int, f.read().split(',')))
 
-print(input_list)
 
-
+rucksacks=[]
 for x in input_list:
-    
-    sum=0
+    rucksacks.append([x[:int(len(x)/2)],x[int(len(x)/2):]])
 
+numbers=[]
 
+for rucksack in rucksacks:
+    character=''
+    for x in rucksack[0]:
+        if x in rucksack[1] and x!=character:
+            character=x
+            if x.isupper():
+                numbers.append(ord(x)-38)
+            if x.islower():
+                numbers.append(ord(x)-96)
+
+print(sum(numbers))
--- Dec03/Task_2.py
+++ Dec03/Task_2.py
@@ -1,7 +1,7 @@
 import os
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-print(os.path.join(__location__,"Input.txt"))
+#
 f = open(os.path.join(__location__,"Input.txt"))
 
 # if test needed
@@ -14,11 +14,17 @@ input_list = f.read().split("\n")
 
 #input_list = list(map(int, f.read().split(',')))
 
-print(input_list)
-
-
-for x in input_list:
-    
-    sum=0
+numbers=[]
+for y in range(0,len(input_list)-2,3):
+    character=''
+    for x in input_list[y]:
+        if x in input_list[y+1] and x != character:
+            if x in input_list[y+2]:
+                character=x
+                if x.isupper():
+                    numbers.append(ord(x)-38)
+                if x.islower():
+                    numbers.append(ord(x)-96)
+print(sum(numbers))
 
 
