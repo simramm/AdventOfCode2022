2f8af8a63eea8c2e2c69eb1847748e9a90d4b99a
simramm <66185123+simramm@users.noreply.github.com>
2022-12-05 06:59:26 +0100

    Completed Dec05 (not great, not terrible)

--- Dec05/Task_2.py
+++ Dec05/Task_2.py
@@ -3,21 +3,37 @@ import os
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 f = open(os.path.join(__location__,"Input.txt"))
 
-# if test needed
-#f = open(os.path.join(__location__,"Input_test.txt"))
-
 input_list = f.read().split("\n")
 
-#input_list = f.read().split()
-#input_list = list(map(int, f.read().split('\n')))
+crates=[]
 
-#input_list = list(map(int, f.read().split(',')))
+crates.append(['R','S','L','F','Q'])
+crates.append(['N','Z','Q','G','P','T'])
+crates.append(['S','M','Q','B'])
+crates.append(['T','G','Z','J','H','C','B','Q'])
+crates.append(['P','H','M','B','N','F','S'])
+crates.append(['P','C','Q','N','S','L','V','G'])
+crates.append(['W','C','F'])
+crates.append(['Q','H','G','Z','W','V','P','M'])
+crates.append(['G','Z','D','L','C','N','R'])
 
-print(input_list)
 
+moves=input_list[10:]
+new_moves=[]
+for move in moves:
+    new_moves.append(move.split(' '))
 
-for x in input_list:
+for move in new_moves:
+    amount = int(move[1])
+    src = int(move[3])-1
+    dst = int(move[5])-1
+
+    crates[dst].extend(crates[src][-amount:])
     
-    sum=0
 
+    crates[src]=crates[src][:-int(amount)]
+
+for crate in crates:
+    print(crate[-1])
+    
 
